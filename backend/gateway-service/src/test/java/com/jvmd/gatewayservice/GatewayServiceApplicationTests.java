8774ee00aac8c37352e84811b0fe50b69ceb23f7
lambdodieno <jvmdevelop@gmail.com>
2026-06-21 10:58:26 +0500

    fix(ci): fix gateway-service test wrong package and add jwt test properties

--- backend/gateway-service/src/test/java/com/jvmd/gatewayservice/GatewayServiceApplicationTests.java
+++ backend/gateway-service/src/test/java/com/jvmd/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jvmd.eurekaserver;
+package com.jvmd.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class GatewayServiceApplicationTests {
 
     @Test
     void contextLoads() {
